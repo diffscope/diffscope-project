79319d40371163d8c8cf74df617957b38d8bcfa8
Crindzebra Sjimo <crs_16423@outlook.com>
2025-09-18 17:27:41 +0800

    New translations welcomewizard_en_us.ts (Japanese)

--- src/plugins/welcomewizard/res/translations/WelcomeWizard_ja_JP.ts
+++ src/plugins/welcomewizard/res/translations/WelcomeWizard_ja_JP.ts
@@ -1,4 +1,25 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_US">
+<TS version="2.1" language="ja_JP" sourcelanguage="en-US">
+  <context>
+    <name>Application::ActionClass</name>
+    <message>
+      <location filename="../../../../../cmake-build-debug/src/plugins/welcomewizard/res/qak_welcome_wizard_actions.cpp" line="125"/>
+      <source>Welcome Wizard</source>
+      <translation type="unfinished">Welcome Wizard</translation>
+    </message>
+  </context>
+  <context>
+    <name>Application::ActionText</name>
+    <message>
+      <location filename="../../../../../cmake-build-debug/src/plugins/welcomewizard/res/qak_welcome_wizard_actions.cpp" line="121"/>
+      <source>Main Menu</source>
+      <translation type="unfinished">Main Menu</translation>
+    </message>
+    <message>
+      <location filename="../../../../../cmake-build-debug/src/plugins/welcomewizard/res/qak_welcome_wizard_actions.cpp" line="122"/>
+      <source>Welcome Wizard</source>
+      <translation type="unfinished">Welcome Wizard</translation>
+    </message>
+  </context>
 </TS>
