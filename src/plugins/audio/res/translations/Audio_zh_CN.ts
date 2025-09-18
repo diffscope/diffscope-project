349cc75f55bc3aaebaa6fb3533c157a4f47f3854
Crindzebra Sjimo <crs_16423@outlook.com>
2025-09-18 17:40:06 +0800

    New translations audio_en_us.ts (Chinese Simplified)

--- src/plugins/audio/res/translations/Audio_zh_CN.ts
+++ src/plugins/audio/res/translations/Audio_zh_CN.ts
@@ -1,4 +1,243 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh_CN">
+<TS version="2.1" language="zh_CN" sourcelanguage="en-US">
+  <context>
+    <name>Application::ActionText</name>
+    <message>
+      <location filename="../../../../../cmake-build-debug/src/plugins/audio/res/qak_audio_actions.cpp" line="78"/>
+      <source>Main Menu</source>
+      <translation type="unfinished">Main Menu</translation>
+    </message>
+  </context>
+  <context>
+    <name>Audio::Internal::AudioAndMidiPage</name>
+    <message>
+      <location filename="../../internal/settings/audioandmidipage.cpp" line="8"/>
+      <source>Audio and MIDI</source>
+      <translation type="unfinished">Audio and MIDI</translation>
+    </message>
+    <message>
+      <location filename="../../internal/settings/audioandmidipage.cpp" line="9"/>
+      <source>Configure audio and MIDI preferences</source>
+      <translation type="unfinished">Configure audio and MIDI preferences</translation>
+    </message>
+  </context>
+  <context>
+    <name>Audio::Internal::AudioOutputPage</name>
+    <message>
+      <location filename="../../internal/settings/audiooutputpage.cpp" line="13"/>
+      <source>Audio Output</source>
+      <translation type="unfinished">Audio Output</translation>
+    </message>
+    <message>
+      <location filename="../../internal/settings/audiooutputpage.cpp" line="14"/>
+      <source>Configure the audio driver and device for output</source>
+      <translation type="unfinished">Configure the audio driver and device for output</translation>
+    </message>
+  </context>
+  <context>
+    <name>Audio::Internal::AudioOutputSettingsHelper</name>
+    <message>
+      <location filename="../../internal/utils/audiooutputsettingshelper.cpp" line="51"/>
+      <location filename="../../internal/utils/audiooutputsettingshelper.cpp" line="65"/>
+      <location filename="../../internal/utils/audiooutputsettingshelper.cpp" line="83"/>
+      <location filename="../../internal/utils/audiooutputsettingshelper.cpp" line="109"/>
+      <location filename="../../internal/utils/audiooutputsettingshelper.cpp" line="119"/>
+      <location filename="../../internal/utils/audiooutputsettingshelper.cpp" line="120"/>
+      <location filename="../../internal/utils/audiooutputsettingshelper.cpp" line="124"/>
+      <location filename="../../internal/utils/audiooutputsettingshelper.cpp" line="131"/>
+      <location filename="../../internal/utils/audiooutputsettingshelper.cpp" line="154"/>
+      <location filename="../../internal/utils/audiooutputsettingshelper.cpp" line="190"/>
+      <location filename="../../internal/utils/audiooutputsettingshelper.cpp" line="212"/>
+      <location filename="../../internal/utils/audiooutputsettingshelper.cpp" line="213"/>
+      <location filename="../../internal/utils/audiooutputsettingshelper.cpp" line="246"/>
+      <location filename="../../internal/utils/audiooutputsettingshelper.cpp" line="247"/>
+      <location filename="../../internal/utils/audiooutputsettingshelper.cpp" line="254"/>
+      <location filename="../../internal/utils/audiooutputsettingshelper.cpp" line="290"/>
+      <location filename="../../internal/utils/audiooutputsettingshelper.cpp" line="308"/>
+      <location filename="../../internal/utils/audiooutputsettingshelper.cpp" line="309"/>
+      <source>(Not working)</source>
+      <translation type="unfinished">(Not working)</translation>
+    </message>
+    <message>
+      <location filename="../../internal/utils/audiooutputsettingshelper.cpp" line="116"/>
+      <source>Cannot initialize %1 driver</source>
+      <translation type="unfinished">Cannot initialize %1 driver</translation>
+    </message>
+    <message>
+      <location filename="../../internal/utils/audiooutputsettingshelper.cpp" line="167"/>
+      <location filename="../../internal/utils/audiooutputsettingshelper.cpp" line="178"/>
+      <location filename="../../internal/utils/audiooutputsettingshelper.cpp" line="200"/>
+      <location filename="../../internal/utils/audiooutputsettingshelper.cpp" line="212"/>
+      <location filename="../../internal/utils/audiooutputsettingshelper.cpp" line="234"/>
+      <location filename="../../internal/utils/audiooutputsettingshelper.cpp" line="246"/>
+      <location filename="../../internal/utils/audiooutputsettingshelper.cpp" line="298"/>
+      <source>Default device</source>
+      <translation type="unfinished">Default device</translation>
+    </message>
+    <message>
+      <location filename="../../internal/utils/audiooutputsettingshelper.cpp" line="298"/>
+      <location filename="../../internal/utils/audiooutputsettingshelper.cpp" line="299"/>
+      <source>Audio device %1 is not available</source>
+      <translation type="unfinished">Audio device %1 is not available</translation>
+    </message>
+    <message>
+      <location filename="../../internal/utils/audiooutputsettingshelper.cpp" line="468"/>
+      <source>Cannot set sample rate to %1</source>
+      <translation type="unfinished">Cannot set sample rate to %1</translation>
+    </message>
+    <message>
+      <location filename="../../internal/utils/audiooutputsettingshelper.cpp" line="499"/>
+      <source>Cannot set buffer size to %1</source>
+      <translation type="unfinished">Cannot set buffer size to %1</translation>
+    </message>
+  </context>
+  <context>
+    <name>Audio::Internal::AudioPlugin</name>
+    <message>
+      <location filename="../../internal/audioplugin.cpp" line="25"/>
+      <source>Initializing audio plugin...</source>
+      <translation type="unfinished">Initializing audio plugin...</translation>
+    </message>
+    <message>
+      <location filename="../../internal/audioplugin.cpp" line="49"/>
+      <source>Failed to initialize audio output system</source>
+      <translation type="unfinished">Failed to initialize audio output system</translation>
+    </message>
+    <message>
+      <location filename="../../internal/audioplugin.cpp" line="50"/>
+      <source>%1 will not play sound because no available audio output device found.
+
+Please go to Settings &gt; Audio and MIDI &gt; Audio Output to check the device status.</source>
+      <translation type="unfinished">%1 will not play sound because no available audio output device found.
+
+Please go to Settings &gt; Audio and MIDI &gt; Audio Output to check the device status.</translation>
+    </message>
+  </context>
+  <context>
+    <name>AudioOutputPage</name>
+    <message>
+      <location filename="../../qml/settings/AudioOutputPage.qml" line="28"/>
+      <source>Device Parameters</source>
+      <translation type="unfinished">Device Parameters</translation>
+    </message>
+    <message>
+      <location filename="../../qml/settings/AudioOutputPage.qml" line="38"/>
+      <source>Audio driver</source>
+      <translation type="unfinished">Audio driver</translation>
+    </message>
+    <message>
+      <location filename="../../qml/settings/AudioOutputPage.qml" line="46"/>
+      <source>Audio device</source>
+      <translation type="unfinished">Audio device</translation>
+    </message>
+    <message>
+      <location filename="../../qml/settings/AudioOutputPage.qml" line="54"/>
+      <source>Sample rate</source>
+      <translation type="unfinished">Sample rate</translation>
+    </message>
+    <message>
+      <location filename="../../qml/settings/AudioOutputPage.qml" line="62"/>
+      <source>Buffer size</source>
+      <translation type="unfinished">Buffer size</translation>
+    </message>
+    <message>
+      <location filename="../../qml/settings/AudioOutputPage.qml" line="74"/>
+      <source>Test</source>
+      <translation type="unfinished">Test</translation>
+    </message>
+    <message>
+      <location filename="../../qml/settings/AudioOutputPage.qml" line="77"/>
+      <source>Open Control Panel</source>
+      <translation type="unfinished">Open Control Panel</translation>
+    </message>
+    <message>
+      <location filename="../../qml/settings/AudioOutputPage.qml" line="83"/>
+      <source>Mixer</source>
+      <translation type="unfinished">Mixer</translation>
+    </message>
+    <message>
+      <location filename="../../qml/settings/AudioOutputPage.qml" line="90"/>
+      <source>Device gain</source>
+      <translation type="unfinished">Device gain</translation>
+    </message>
+    <message>
+      <location filename="../../qml/settings/AudioOutputPage.qml" line="103"/>
+      <source>Device pan</source>
+      <translation type="unfinished">Device pan</translation>
+    </message>
+    <message>
+      <location filename="../../qml/settings/AudioOutputPage.qml" line="118"/>
+      <source>Hot Plug</source>
+      <translation type="unfinished">Hot Plug</translation>
+    </message>
+    <message>
+      <location filename="../../qml/settings/AudioOutputPage.qml" line="124"/>
+      <source>When device change detected</source>
+      <translation type="unfinished">When device change detected</translation>
+    </message>
+    <message>
+      <location filename="../../qml/settings/AudioOutputPage.qml" line="130"/>
+      <source>Always show notification</source>
+      <translation type="unfinished">Always show notification</translation>
+    </message>
+    <message>
+      <location filename="../../qml/settings/AudioOutputPage.qml" line="131"/>
+      <source>Show notification only when the current device is removed</source>
+      <translation type="unfinished">Show notification only when the current device is removed</translation>
+    </message>
+    <message>
+      <location filename="../../qml/settings/AudioOutputPage.qml" line="132"/>
+      <source>Do not show notification</source>
+      <translation type="unfinished">Do not show notification</translation>
+    </message>
+  </context>
+  <context>
+    <name>AudioOutputWelcomeWizardPage</name>
+    <message>
+      <location filename="../../qml/welcomewizardpages/AudioOutputWelcomeWizardPage.qml" line="17"/>
+      <source>Audio Output</source>
+      <translation type="unfinished">Audio Output</translation>
+    </message>
+    <message>
+      <location filename="../../qml/welcomewizardpages/AudioOutputWelcomeWizardPage.qml" line="18"/>
+      <source>Configure audio output device parameters</source>
+      <translation type="unfinished">Configure audio output device parameters</translation>
+    </message>
+    <message>
+      <location filename="../../qml/welcomewizardpages/AudioOutputWelcomeWizardPage.qml" line="32"/>
+      <source>Audio driver</source>
+      <translation type="unfinished">Audio driver</translation>
+    </message>
+    <message>
+      <location filename="../../qml/welcomewizardpages/AudioOutputWelcomeWizardPage.qml" line="39"/>
+      <source>Audio device</source>
+      <translation type="unfinished">Audio device</translation>
+    </message>
+    <message>
+      <location filename="../../qml/welcomewizardpages/AudioOutputWelcomeWizardPage.qml" line="46"/>
+      <source>Sample rate</source>
+      <translation type="unfinished">Sample rate</translation>
+    </message>
+    <message>
+      <location filename="../../qml/welcomewizardpages/AudioOutputWelcomeWizardPage.qml" line="53"/>
+      <source>Buffer size</source>
+      <translation type="unfinished">Buffer size</translation>
+    </message>
+    <message>
+      <location filename="../../qml/welcomewizardpages/AudioOutputWelcomeWizardPage.qml" line="64"/>
+      <source>Test</source>
+      <translation type="unfinished">Test</translation>
+    </message>
+    <message>
+      <location filename="../../qml/welcomewizardpages/AudioOutputWelcomeWizardPage.qml" line="67"/>
+      <source>Open Control Panel</source>
+      <translation type="unfinished">Open Control Panel</translation>
+    </message>
+    <message>
+      <location filename="../../qml/welcomewizardpages/AudioOutputWelcomeWizardPage.qml" line="73"/>
+      <source>Click &quot;Test&quot; button to check whether audio output is configured correctly</source>
+      <translation type="unfinished">Click &quot;Test&quot; button to check whether audio output is configured correctly</translation>
+    </message>
+  </context>
 </TS>
