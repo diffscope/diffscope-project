777cd9cac734165ecb6798dce35dd88b671631e9
CrSjimo <crs_16423@outlook.com>
2025-09-25 00:15:18 +0800

    update install

--- src/share/install.cmake
+++ src/share/install.cmake
@@ -5,71 +5,35 @@ install(DIRECTORY ${CK_BUILD_MAIN_DIR}/share/
 
 qm_find_qt(Core)
 
-set(_plugins)
+set(_plugins
+    generic/qtuiotouchplugin
+    iconengines/qsvgicon
+    imageformats/q*
+    networkinformation/qnetworklistmanager
+)
 
-if(QT_VERSION_MAJOR EQUAL 5)
-    # Qt5
+if(WIN32)
+    # Windows
     list(APPEND _plugins
-        bearer/qgenericbearer
-        iconengines/qsvgicon
-        imageformats/q*
+        platforms/qwindows
+        styles/qmodernwindowsstyle
     )
-
-    if(WIN32)
-        # Windows
-        list(APPEND _plugins
-            platforms/qwindows
-            styles/qwindowsvistastyle
-        )
-    elseif(APPLE)
-        # Mac
-        list(APPEND _plugins
-            platforms/qcocoa
-            styles/qmacstyle
-            platforminputcontexts/q*
-            printsupport/q*
-            virtualkeyboard/q*
-        )
-    else()
-        # Linux
-        list(APPEND _plugins
-            platforms/qxcb
-            platforminputcontexts/*
-            xcbglintegrations/*
-        )
-    endif()
+elseif(APPLE)
+    # Mac
+    list(APPEND _plugins
+        platforms/qcocoa
+        styles/qmacstyle
+        platforminputcontexts/q*
+        printsupport/q*
+        virtualkeyboard/q*
+    )
 else()
-    # Qt6
-    set(_plugins
-        generic/qtuiotouchplugin
-        iconengines/qsvgicon
-        imageformats/q*
-        networkinformation/qnetworklistmanager
+    # Linux
+    list(APPEND _plugins
+        platforms/qxcb
+        platforminputcontexts/*
+        xcbglintegrations/*
     )
-
-    if(WIN32)
-        # Windows
-        list(APPEND _plugins
-            platforms/qwindows
-            styles/qmodernwindowsstyle
-        )
-    elseif(APPLE)
-        # Mac
-        list(APPEND _plugins
-            platforms/qcocoa
-            styles/qmacstyle
-            platforminputcontexts/q*
-            printsupport/q*
-            virtualkeyboard/q*
-        )
-    else()
-        # Linux
-        list(APPEND _plugins
-            platforms/qxcb
-            platforminputcontexts/*
-            xcbglintegrations/*
-        )
-    endif()
 endif()
 
 if(WIN32)
@@ -84,11 +48,21 @@ else()
     set(_extra_search_path ${CK_BUILD_LIBRARY_DIR})
 endif()
 
+set(_qml
+    Qt/labs
+    Qt5Compat
+    QtCore
+    QtQml
+    QtQuick
+)
+
 qm_deploy_directory(${CMAKE_INSTALL_PREFIX}
     PLUGINS ${_plugins}
     LIBRARY_DIR ${_lib_dir}
     PLUGIN_DIR ${CK_INSTALL_LIBRARY_DIR}/Qt/plugins
     EXTRA_SEARCHING_PATHS ${_extra_search_path}
+    QML ${_qml}
+    QML_DIR ${CK_INSTALL_QML_DIR}
     VERBOSE
 )
 
