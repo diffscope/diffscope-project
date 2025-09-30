b8256e1b4c10cd464bee6417dc0f763f72c68743
Crindzebra Sjimo <crs_16423@outlook.com>
2025-09-30 14:51:12 +0800

    New translations welcomewizard_en_us.ts (Chinese Simplified)

--- src/plugins/welcomewizard/res/translations/WelcomeWizard_zh_CN.ts
+++ src/plugins/welcomewizard/res/translations/WelcomeWizard_zh_CN.ts
@@ -1,25 +1,25 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="zh_CN" sourcelanguage="en-US">
-<context>
+  <context>
     <name>Application::ActionClass</name>
     <message>
-        <location filename="../../../../../cmake-build-debug/src/plugins/welcomewizard/res/qak_welcome_wizard_actions.cpp" line="125"/>
-        <source>Welcome Wizard</source>
-        <translation>欢迎向导</translation>
+      <location filename="../../../../../cmake-build-debug/src/plugins/welcomewizard/res/qak_welcome_wizard_actions.cpp" line="125"/>
+      <source>Welcome Wizard</source>
+      <translation>欢迎向导</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>Application::ActionText</name>
     <message>
-        <location filename="../../../../../cmake-build-debug/src/plugins/welcomewizard/res/qak_welcome_wizard_actions.cpp" line="121"/>
-        <source>Main Menu</source>
-        <translation>主菜单</translation>
+      <location filename="../../../../../cmake-build-debug/src/plugins/welcomewizard/res/qak_welcome_wizard_actions.cpp" line="121"/>
+      <source>Main Menu</source>
+      <translation>主菜单</translation>
     </message>
     <message>
-        <location filename="../../../../../cmake-build-debug/src/plugins/welcomewizard/res/qak_welcome_wizard_actions.cpp" line="122"/>
-        <source>Welcome Wizard</source>
-        <translation>欢迎向导</translation>
+      <location filename="../../../../../cmake-build-debug/src/plugins/welcomewizard/res/qak_welcome_wizard_actions.cpp" line="122"/>
+      <source>Welcome Wizard</source>
+      <translation>欢迎向导</translation>
     </message>
-</context>
+  </context>
 </TS>
