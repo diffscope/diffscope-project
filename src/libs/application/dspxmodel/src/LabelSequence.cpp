9df14b8afd6bbebb7ee81ec645f9c09d39d33fef
CrSjimo <crs_16423@outlook.com>
2025-10-15 00:50:09 +0800

    Make PointSequenceData a base class for the private classes of point sequence entity objects

--- src/libs/application/dspxmodel/src/LabelSequence.cpp
+++ src/libs/application/dspxmodel/src/LabelSequence.cpp
@@ -9,94 +9,13 @@
 #include <dspxmodel/private/Model_p.h>
 #include <dspxmodel/Label.h>
 #include <dspxmodel/ModelStrategy.h>
+#include <dspxmodel/private/PointSequenceData_p.h>
 
 namespace dspx {
 
-    class LabelSequencePrivate {
+    class LabelSequencePrivate : public PointSequenceData<LabelSequence, Label, &Label::pos, &Label::posChanged> {
         Q_DECLARE_PUBLIC(LabelSequence)
-    public:
-        LabelSequence *q_ptr;
-        ModelPrivate *pModel;
-        PointSequenceContainer<Label> container;
-        Label *firstItem{};
-        Label *lastItem{};
-        QJSValue iterable_;
-
-        Label *getItem(Handle handle, bool create) const;
-
-        void insertItem(Label *item, int position);
-        void removeItem(Label *item);
-        void updateFirstAndLastItem();
-
-        QJSValue iterable();
     };
-
-    QJSValue LabelSequencePrivate::iterable() {
-        Q_Q(LabelSequence);
-        if (!iterable_.isUndefined()) {
-            return iterable_;
-        }
-        auto engine = qjsEngine(q);
-        if (!engine) {
-            qFatal() << "iterable() can only be called from QML";
-            return {};
-        }
-        auto sequenceIterableConstructor = engine->importModule(":/qt/qml/DiffScope/DspxModel/qml/iterable.mjs").property("SequenceIterable");
-        Q_ASSERT(sequenceIterableConstructor.isCallable());
-        iterable_ = sequenceIterableConstructor.callAsConstructor({engine->fromVariant<QJSValue>(QVariant::fromValue(q))});
-        if (iterable_.isError()) {
-            qFatal() << iterable_.property("message").toString();
-            return {};
-        }
-        return iterable_;
-    }
-
-    Label *LabelSequencePrivate::getItem(Handle handle, bool create) const {
-        if (auto object = pModel->mapToObject(handle)) {
-            auto label = qobject_cast<Label *>(object);
-            Q_ASSERT(label);
-            return label;
-        }
-        if (create) {
-            return pModel->createObject<Label>(handle);
-        }
-        Q_UNREACHABLE();
-    }
-
-    void LabelSequencePrivate::insertItem(Label *item, int position) {
-        Q_Q(LabelSequence);
-        bool containsItem = container.contains(item);
-        if (!containsItem) {
-            Q_EMIT q->itemAboutToInsert(item);
-        }
-        container.insertItem(item, position);
-        if (!containsItem) {
-            updateFirstAndLastItem();
-            Q_EMIT q->itemInserted(item);
-            Q_EMIT q->sizeChanged(container.size());
-        }
-    }
-
-    void LabelSequencePrivate::removeItem(Label *item) {
-        Q_Q(LabelSequence);
-        Q_EMIT q->itemAboutToRemove(item);
-        container.removeItem(item);
-        updateFirstAndLastItem();
-        Q_EMIT q->itemRemoved(item);
-        Q_EMIT q->sizeChanged(container.size());
-    }
-
-    void LabelSequencePrivate::updateFirstAndLastItem() {
-        Q_Q(LabelSequence);
-        if (auto a = container.firstItem(); firstItem != a) {
-            firstItem = a;
-            Q_EMIT q->firstItemChanged(firstItem);
-        }
-        if (auto a = container.lastItem(); lastItem != a) {
-            lastItem = a;
-            Q_EMIT q->lastItemChanged(lastItem);
-        }
-    }
 
     LabelSequence::LabelSequence(Handle handle, Model *model) : EntityObject(handle, model), d_ptr(new LabelSequencePrivate) {
         Q_D(LabelSequence);
@@ -174,21 +93,12 @@ namespace dspx {
 
     void LabelSequence::handleInsertIntoSequenceContainer(Handle entity) {
         Q_D(LabelSequence);
-        auto label = d->getItem(entity, true);
-        connect(label, &Label::posChanged, this, [=](int pos) {
-            d->insertItem(label, pos);
-        });
-        connect(label, &Label::destroyed, this, [=] {
-            d->removeItem(label);
-        });
-        d->insertItem(label, label->pos());
+        d->handleInsertIntoSequenceContainer(entity);
     }
 
     void LabelSequence::handleTakeFromSequenceContainer(Handle takenEntity, Handle entity) {
         Q_D(LabelSequence);
-        auto label = d->getItem(takenEntity, false);
-        disconnect(label, nullptr, this, nullptr);
-        d->removeItem(label);
+        d->handleTakeFromSequenceContainer(takenEntity, entity);
     }
 
 }
