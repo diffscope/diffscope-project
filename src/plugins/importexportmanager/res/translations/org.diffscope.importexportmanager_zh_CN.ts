069bda70eeffcb77b8a95101d1a0b0a3e01ccea7
Crindzebra Sjimo <crs_16423@outlook.com>
2025-12-26 18:44:00 +0800

    New translations org.diffscope.importexportmanager_en_us.ts (Chinese Simplified)

--- src/plugins/importexportmanager/res/translations/org.diffscope.importexportmanager_zh_CN.ts
+++ src/plugins/importexportmanager/res/translations/org.diffscope.importexportmanager_zh_CN.ts
@@ -6,7 +6,7 @@
     <message>
       <location filename="../../../../../cmake-build-debug/src/plugins/importexportmanager/res/qak_org.diffscope.importexportmanager_actions.cpp" line="401"/>
       <source>Main Menu</source>
-      <translation type="unfinished">Main Menu</translation>
+      <translation>主菜单</translation>
     </message>
   </context>
   <context>
@@ -14,12 +14,12 @@
     <message>
       <location filename="../../../../app/main.cpp" line="80"/>
       <source>Version %1</source>
-      <translation type="unfinished">Version %1</translation>
+      <translation>版本 %1</translation>
     </message>
     <message>
       <location filename="../../../../app/main.cpp" line="81"/>
       <source>Copyright © %1-%2 %3. All rights reserved.</source>
-      <translation type="unfinished">Copyright © %1-%2 %3. All rights reserved.</translation>
+      <translation>版权所有 © %1-%2 %3。保留所有权利。</translation>
     </message>
   </context>
   <context>
@@ -28,23 +28,23 @@
       <location filename="../../../../../cmake-build-debug/src/plugins/importexportmanager/res/qak_org.diffscope.importexportmanager_actions.cpp" line="412"/>
       <location filename="../../../../../cmake-build-debug/src/plugins/importexportmanager/res/qak_org.diffscope.importexportmanager_actions.cpp" line="413"/>
       <source>File</source>
-      <translation type="unfinished">File</translation>
+      <translation>文件</translation>
     </message>
     <message>
       <location filename="../../../../../cmake-build-debug/src/plugins/importexportmanager/res/qak_org.diffscope.importexportmanager_actions.cpp" line="414"/>
       <source>Project</source>
-      <translation type="unfinished">Project</translation>
+      <translation>工程</translation>
     </message>
     <message>
       <location filename="../../../../../cmake-build-debug/src/plugins/importexportmanager/res/qak_org.diffscope.importexportmanager_actions.cpp" line="415"/>
       <location filename="../../../../../cmake-build-debug/src/plugins/importexportmanager/res/qak_org.diffscope.importexportmanager_actions.cpp" line="416"/>
       <source>Edit</source>
-      <translation type="unfinished">Edit</translation>
+      <translation>编辑</translation>
     </message>
     <message>
       <location filename="../../../../../cmake-build-debug/src/plugins/importexportmanager/res/qak_org.diffscope.importexportmanager_actions.cpp" line="417"/>
       <source>Panel</source>
-      <translation type="unfinished">Panel</translation>
+      <translation>面板</translation>
     </message>
   </context>
   <context>
@@ -52,42 +52,42 @@
     <message>
       <location filename="../../../../../cmake-build-debug/src/plugins/importexportmanager/res/qak_org.diffscope.importexportmanager_actions.cpp" line="402"/>
       <source>&amp;Import</source>
-      <translation type="unfinished">&amp;Import</translation>
+      <translation>导入(&amp;I)</translation>
     </message>
     <message>
       <location filename="../../../../../cmake-build-debug/src/plugins/importexportmanager/res/qak_org.diffscope.importexportmanager_actions.cpp" line="403"/>
       <source>&amp;Export</source>
-      <translation type="unfinished">&amp;Export</translation>
+      <translation>导出(&amp;E)</translation>
     </message>
     <message>
       <location filename="../../../../../cmake-build-debug/src/plugins/importexportmanager/res/qak_org.diffscope.importexportmanager_actions.cpp" line="404"/>
       <source>Import as Tracks...</source>
-      <translation type="unfinished">Import as Tracks...</translation>
+      <translation>导入为轨道...</translation>
     </message>
     <message>
       <location filename="../../../../../cmake-build-debug/src/plugins/importexportmanager/res/qak_org.diffscope.importexportmanager_actions.cpp" line="405"/>
       <source>Copy Special...</source>
-      <translation type="unfinished">Copy Special...</translation>
+      <translation>选择性复制...</translation>
     </message>
     <message>
       <location filename="../../../../../cmake-build-debug/src/plugins/importexportmanager/res/qak_org.diffscope.importexportmanager_actions.cpp" line="406"/>
       <source>Paste Special...</source>
-      <translation type="unfinished">Paste Special...</translation>
+      <translation>选择性粘贴...</translation>
     </message>
     <message>
       <location filename="../../../../../cmake-build-debug/src/plugins/importexportmanager/res/qak_org.diffscope.importexportmanager_actions.cpp" line="407"/>
       <source>Import</source>
-      <translation type="unfinished">Import</translation>
+      <translation>导入</translation>
     </message>
     <message>
       <location filename="../../../../../cmake-build-debug/src/plugins/importexportmanager/res/qak_org.diffscope.importexportmanager_actions.cpp" line="408"/>
       <source>Import Export Actions</source>
-      <translation type="unfinished">Import Export Actions</translation>
+      <translation>导入导出操作</translation>
     </message>
     <message>
       <location filename="../../../../../cmake-build-debug/src/plugins/importexportmanager/res/qak_org.diffscope.importexportmanager_actions.cpp" line="409"/>
       <source>Import Actions</source>
-      <translation type="unfinished">Import Actions</translation>
+      <translation>导入操作</translation>
     </message>
   </context>
   <context>
@@ -95,42 +95,42 @@
     <message>
       <location filename="../../internal/fileconverters/DspxFileExporter.cpp" line="30"/>
       <source>DSPX</source>
-      <translation type="unfinished">DSPX</translation>
+      <translation>DSPX</translation>
     </message>
     <message>
       <location filename="../../internal/fileconverters/DspxFileExporter.cpp" line="31"/>
       <source>Export as DSPX file compatible with older versions of DiffScope and other editors</source>
-      <translation type="unfinished">Export as DSPX file compatible with older versions of DiffScope and other editors</translation>
+      <translation>导出为与旧版 DiffScope 和其他编辑器兼容的 DSPX 文件</translation>
     </message>
     <message>
       <location filename="../../internal/fileconverters/DspxFileExporter.cpp" line="32"/>
       <source>DiffScope Project Exchange Format (*.dspx)</source>
-      <translation type="unfinished">DiffScope Project Exchange Format (*.dspx)</translation>
+      <translation>DiffScope 工程交换格式 (*.dspx)</translation>
     </message>
     <message>
       <location filename="../../internal/fileconverters/DspxFileExporter.cpp" line="43"/>
       <source>Export DSPX</source>
-      <translation type="unfinished">Export DSPX</translation>
+      <translation>导出 DSPX</translation>
     </message>
     <message>
       <location filename="../../internal/fileconverters/DspxFileExporter.cpp" line="48"/>
       <source>Format version</source>
-      <translation type="unfinished">Format version</translation>
+      <translation>格式版本</translation>
     </message>
     <message>
       <location filename="../../internal/fileconverters/DspxFileExporter.cpp" line="49"/>
       <source>Keep Workspace data</source>
-      <translation type="unfinished">Keep Workspace data</translation>
+      <translation>保留“Workspace”数据</translation>
     </message>
     <message>
       <location filename="../../internal/fileconverters/DspxFileExporter.cpp" line="52"/>
       <source>Workspace data is not part of the DSPX standard and is editor-specific. Different editors may interpret the same fields differently, which can potentially lead to unexpected behavior or compatibility issues.</source>
-      <translation type="unfinished">Workspace data is not part of the DSPX standard and is editor-specific. Different editors may interpret the same fields differently, which can potentially lead to unexpected behavior or compatibility issues.</translation>
+      <translation>“Workspace”数据不是 DSPX 标准的一部分，而特定于编辑器。不同的编辑器可能使用不同的方式解读同一字段，这会导致潜在的意外行为或兼容性问题。</translation>
     </message>
     <message>
       <location filename="../../internal/fileconverters/DspxFileExporter.cpp" line="59"/>
       <source>Export</source>
-      <translation type="unfinished">Export</translation>
+      <translation>导出</translation>
     </message>
   </context>
   <context>
@@ -138,37 +138,37 @@
     <message>
       <location filename="../../internal/fileconverters/DspxFileImporter.cpp" line="28"/>
       <source>DSPX</source>
-      <translation type="unfinished">DSPX</translation>
+      <translation>DSPX</translation>
     </message>
     <message>
       <location filename="../../internal/fileconverters/DspxFileImporter.cpp" line="29"/>
       <source>Import DSPX file compatible with DiffScope</source>
-      <translation type="unfinished">Import DSPX file compatible with DiffScope</translation>
+      <translation>导入适用于 DiffScope 的 DSPX 文件</translation>
     </message>
     <message>
       <location filename="../../internal/fileconverters/DspxFileImporter.cpp" line="30"/>
       <source>DiffScope Project Exchange Format (*.dspx)</source>
-      <translation type="unfinished">DiffScope Project Exchange Format (*.dspx)</translation>
+      <translation>DiffScope 工程交换格式 (*.dspx)</translation>
     </message>
     <message>
       <location filename="../../internal/fileconverters/DspxFileImporter.cpp" line="55"/>
       <source>Import DSPX</source>
-      <translation type="unfinished">Import DSPX</translation>
+      <translation>导入 DSPX</translation>
     </message>
     <message>
       <location filename="../../internal/fileconverters/DspxFileImporter.cpp" line="57"/>
       <source>Keep Workspace data</source>
-      <translation type="unfinished">Keep Workspace data</translation>
+      <translation>保留“Workspace”数据</translation>
     </message>
     <message>
       <location filename="../../internal/fileconverters/DspxFileImporter.cpp" line="60"/>
       <source>Workspace data is not part of the DSPX standard and is editor-specific. Different editors may interpret the same fields differently, which can potentially lead to unexpected behavior or compatibility issues.</source>
-      <translation type="unfinished">Workspace data is not part of the DSPX standard and is editor-specific. Different editors may interpret the same fields differently, which can potentially lead to unexpected behavior or compatibility issues.</translation>
+      <translation>“Workspace”数据不是 DSPX 标准的一部分，而特定于编辑器。不同的编辑器可能使用不同的方式解读同一字段，这会导致潜在的意外行为或兼容性问题。</translation>
     </message>
     <message>
       <location filename="../../internal/fileconverters/DspxFileImporter.cpp" line="66"/>
       <source>Import</source>
-      <translation type="unfinished">Import</translation>
+      <translation>导入</translation>
     </message>
   </context>
   <context>
@@ -176,12 +176,12 @@
     <message>
       <location filename="../../internal/addon/FileImportExportAddOn.cpp" line="135"/>
       <source>Import File</source>
-      <translation type="unfinished">Import File</translation>
+      <translation>导入文件</translation>
     </message>
     <message>
       <location filename="../../internal/addon/FileImportExportAddOn.cpp" line="166"/>
       <source>Export File</source>
-      <translation type="unfinished">Export File</translation>
+      <translation>导出文件</translation>
     </message>
   </context>
   <context>
@@ -189,7 +189,7 @@
     <message>
       <location filename="../../qml/panels/ImportPanel.qml" line="30"/>
       <source>Select a format to import</source>
-      <translation type="unfinished">Select a format to import</translation>
+      <translation>选择要导入的格式</translation>
     </message>
   </context>
 </TS>
