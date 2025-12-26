b79ef86bf4fd6da51b06cbacbdacbc72aec048be
Crindzebra Sjimo <crs_16423@outlook.com>
2025-12-26 17:52:27 +0800

    New translations org.diffscope.libresvipformatconverter_en_us.ts (Chinese Simplified)

--- src/plugins/libresvipformatconverter/res/translations/org.diffscope.libresvipformatconverter_zh_CN.ts
+++ src/plugins/libresvipformatconverter/res/translations/org.diffscope.libresvipformatconverter_zh_CN.ts
@@ -1,4 +1,43 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="zh_CN" sourcelanguage="en-US">
+  <context>
+    <name>Application</name>
+    <message>
+      <location filename="../../../../app/main.cpp" line="80"/>
+      <source>Version %1</source>
+      <translation type="unfinished">Version %1</translation>
+    </message>
+    <message>
+      <location filename="../../../../app/main.cpp" line="81"/>
+      <source>Copyright © %1-%2 %3. All rights reserved.</source>
+      <translation type="unfinished">Copyright © %1-%2 %3. All rights reserved.</translation>
+    </message>
+  </context>
+  <context>
+    <name>LibreSVIPFormatConverter::Internal::LibreSVIPFileExporter</name>
+    <message>
+      <location filename="../../internal/LibreSVIPFileExporter.cpp" line="10"/>
+      <source>Multiple formats (export via LibreSVIP)</source>
+      <translation type="unfinished">Multiple formats (export via LibreSVIP)</translation>
+    </message>
+    <message>
+      <location filename="../../internal/LibreSVIPFileExporter.cpp" line="11"/>
+      <source>Export as project files of other editors (such as VOCALOID, OpenUtau, etc.) via LibreSVIP</source>
+      <translation type="unfinished">Export as project files of other editors (such as VOCALOID, OpenUtau, etc.) via LibreSVIP</translation>
+    </message>
+  </context>
+  <context>
+    <name>LibreSVIPFormatConverter::Internal::LibreSVIPFileImporter</name>
+    <message>
+      <location filename="../../internal/LibreSVIPFileImporter.cpp" line="10"/>
+      <source>Multiple formats (import via LibreSVIP)</source>
+      <translation type="unfinished">Multiple formats (import via LibreSVIP)</translation>
+    </message>
+    <message>
+      <location filename="../../internal/LibreSVIPFileImporter.cpp" line="11"/>
+      <source>Import project files of other editors (such as VOCALOID, OpenUtau, etc.) via LibreSVIP</source>
+      <translation type="unfinished">Import project files of other editors (such as VOCALOID, OpenUtau, etc.) via LibreSVIP</translation>
+    </message>
+  </context>
 </TS>
