84527acb62fc2f40e72fe5b39814d8d67d6a7a51
Crindzebra Sjimo <crs_16423@outlook.com>
2025-12-26 16:28:49 +0800

    Update source file org.diffscope.visualeditor_en_US.ts

--- src/plugins/visualeditor/res/translations/org.diffscope.visualeditor_en_US.ts
+++ src/plugins/visualeditor/res/translations/org.diffscope.visualeditor_en_US.ts
@@ -1,4 +1,438 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US" sourcelanguage="en-US">
+<context>
+    <name></name>
+    <message>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="687"/>
+        <source>Main Menu</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Application</name>
+    <message>
+        <location filename="../../../../app/main.cpp" line="80"/>
+        <source>Version %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../app/main.cpp" line="81"/>
+        <source>Copyright © %1-%2 %3. All rights reserved.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Application::ActionClass</name>
+    <message>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="709"/>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="710"/>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="711"/>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="712"/>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="713"/>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="714"/>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="715"/>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="716"/>
+        <source>Arrangement Panel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="717"/>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="718"/>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="719"/>
+        <source>Panel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="720"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Application::ActionText</name>
+    <message>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="688"/>
+        <source>Pointer Tool</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="689"/>
+        <source>Pencil Tool</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="690"/>
+        <source>Hand Tool</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="691"/>
+        <source>Snap</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="692"/>
+        <source>Snap Duration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="693"/>
+        <source>Snap Tuplet</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="694"/>
+        <source>Auto Page Scrolling</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="695"/>
+        <source>Additional Tracks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="696"/>
+        <source>Arrangement</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="697"/>
+        <source>Mixer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="698"/>
+        <source>Piano Roll</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="699"/>
+        <source>Additional Track Widgets</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="700"/>
+        <source>Label</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="701"/>
+        <source>Tempo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="702"/>
+        <source>Arrangement Panel Tool Bar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="703"/>
+        <source>Tool Actions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="704"/>
+        <source>Snap Actions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="705"/>
+        <source>Arrangement Panel Timeline Tool Bar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../../cmake-build-debug/src/plugins/visualeditor/res/qak_org.diffscope.visualeditor_actions.cpp" line="706"/>
+        <source>New</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ArrangementAddOnActions</name>
+    <message>
+        <location filename="../../qml/actions/ArrangementAddOnActions.qml" line="69"/>
+        <source>Auto</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/actions/ArrangementAddOnActions.qml" line="70"/>
+        <location filename="../../qml/actions/ArrangementAddOnActions.qml" line="108"/>
+        <source>None</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/actions/ArrangementAddOnActions.qml" line="71"/>
+        <source>Whole note</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/actions/ArrangementAddOnActions.qml" line="72"/>
+        <source>Half note</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/actions/ArrangementAddOnActions.qml" line="73"/>
+        <source>Quarter note</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/actions/ArrangementAddOnActions.qml" line="74"/>
+        <source>8th note</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/actions/ArrangementAddOnActions.qml" line="75"/>
+        <source>16th note</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/actions/ArrangementAddOnActions.qml" line="76"/>
+        <source>32nd note</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/actions/ArrangementAddOnActions.qml" line="77"/>
+        <source>64th note</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/actions/ArrangementAddOnActions.qml" line="78"/>
+        <source>128th note</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/actions/ArrangementAddOnActions.qml" line="109"/>
+        <source>Triplet</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/actions/ArrangementAddOnActions.qml" line="110"/>
+        <source>Quintuplet</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ArrangementView</name>
+    <message>
+        <location filename="../../qml/ArrangementView.qml" line="94"/>
+        <source>Move Up</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/ArrangementView.qml" line="100"/>
+        <source>Move Down</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/ArrangementView.qml" line="105"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>EditorPage</name>
+    <message>
+        <location filename="../../qml/settings/EditorPage.qml" line="47"/>
+        <source>Move and Zoom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/settings/EditorPage.qml" line="56"/>
+        <source>Horizontal scroll</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/settings/EditorPage.qml" line="69"/>
+        <source>Zoom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/settings/EditorPage.qml" line="83"/>
+        <source>Scroll by page</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/settings/EditorPage.qml" line="89"/>
+        <source>Horizontal zoom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/settings/EditorPage.qml" line="104"/>
+        <source>Middle button/hand tool scroll mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/settings/EditorPage.qml" line="111"/>
+        <source>Dragging</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/settings/EditorPage.qml" line="111"/>
+        <source>Auto scrolling</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/settings/EditorPage.qml" line="119"/>
+        <source>Snap</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/settings/EditorPage.qml" line="130"/>
+        <source>Auto-snap length</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/settings/EditorPage.qml" line="154"/>
+        <source>Temporarily disable snap when pressing %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/settings/EditorPage.qml" line="164"/>
+        <source>Miscellaneous</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/settings/EditorPage.qml" line="175"/>
+        <source>Track cursor position</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SnapControl</name>
+    <message>
+        <location filename="../../qml/SnapControl.qml" line="21"/>
+        <source>Snap</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/SnapControl.qml" line="27"/>
+        <source>Auto</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/SnapControl.qml" line="28"/>
+        <source>None</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/SnapControl.qml" line="29"/>
+        <source>Whole note</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/SnapControl.qml" line="30"/>
+        <source>Half note</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/SnapControl.qml" line="31"/>
+        <source>Quarter note</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/SnapControl.qml" line="32"/>
+        <source>8th note</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/SnapControl.qml" line="33"/>
+        <source>16th note</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/SnapControl.qml" line="34"/>
+        <source>32nd note</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/SnapControl.qml" line="35"/>
+        <source>64th note</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/SnapControl.qml" line="36"/>
+        <source>128th note</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/SnapControl.qml" line="48"/>
+        <source>Triplet</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/SnapControl.qml" line="61"/>
+        <source>Quintuplet</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TimelineContextMenuHelper</name>
+    <message>
+        <location filename="../../qml/TimelineContextMenuHelper.qml" line="46"/>
+        <source>Insert Time Signature</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/TimelineContextMenuHelper.qml" line="54"/>
+        <location filename="../../qml/TimelineContextMenuHelper.qml" line="99"/>
+        <source>Move Playhead Here</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/TimelineContextMenuHelper.qml" line="71"/>
+        <source>Edit Time Signature</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../qml/TimelineContextMenuHelper.qml" line="77"/>
+        <source>Remove Time Signature</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>VisualEditor::Internal::EditorPage</name>
+    <message>
+        <location filename="../../internal/settings/EditorPage.cpp" line="16"/>
+        <source>Editor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../internal/settings/EditorPage.cpp" line="17"/>
+        <source>Configure editor appearance and interaction behaviors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../internal/settings/EditorPage.cpp" line="98"/>
+        <location filename="../../internal/settings/EditorPage.cpp" line="99"/>
+        <location filename="../../internal/settings/EditorPage.cpp" line="100"/>
+        <source>Scroll</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>VisualEditor::LabelViewModelContextData</name>
+    <message>
+        <location filename="../../core/LabelViewModelContextData.cpp" line="350"/>
+        <source>Moving label</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../core/LabelViewModelContextData.cpp" line="372"/>
+        <source>Editing label</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../core/LabelViewModelContextData.cpp" line="404"/>
+        <source>Inserting label</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>VisualEditor::TempoViewModelContextData</name>
+    <message>
+        <location filename="../../core/TempoViewModelContextData.cpp" line="258"/>
+        <source>Moving tempo</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 </TS>
