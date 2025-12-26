942f95fa0ec2915bf8364c475d69534afa2e5fb3
Crindzebra Sjimo <crs_16423@outlook.com>
2025-12-26 16:26:11 +0800

    New translations org.diffscope.welcomewizard_en_us.ts (Chinese Simplified)

---
+++ zh-CN/src/plugins/welcomewizard/res/translations/org.diffscope.welcomewizard_zh_CN.ts
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN" sourcelanguage="en-US">
+  <context>
+    <name/>
+    <message>
+      <location filename="../../../../../cmake-build-debug/src/plugins/welcomewizard/res/qak_org.diffscope.welcomewizard_actions.cpp" line="124"/>
+      <source>Main Menu</source>
+      <translation type="unfinished">Main Menu</translation>
+    </message>
+  </context>
+  <context>
+    <name>Application</name>
+    <message>
+      <location filename="../../../../app/main.cpp" line="80"/>
+      <source>Version %1</source>
+      <translation type="unfinished">Version %1</translation>
+    </message>
+    <message>
+      <location filename="../../../../app/main.cpp" line="81"/>
+      <source>Copyright © %1-%2 %3. All rights reserved.</source>
+      <translation type="unfinished">Copyright © %1-%2 %3. All rights reserved.</translation>
+    </message>
+  </context>
+  <context>
+    <name>Application::ActionClass</name>
+    <message>
+      <source>Welcome Wizard</source>
+      <translation>欢迎向导</translation>
+    </message>
+    <message>
+      <location filename="../../../../../cmake-build-debug/src/plugins/welcomewizard/res/qak_org.diffscope.welcomewizard_actions.cpp" line="128"/>
+      <source>Welcomewizard</source>
+      <translation type="unfinished">Welcomewizard</translation>
+    </message>
+  </context>
+  <context>
+    <name>Application::ActionText</name>
+    <message>
+      <source>Main Menu</source>
+      <translation>主菜单</translation>
+    </message>
+    <message>
+      <location filename="../../../../../cmake-build-debug/src/plugins/welcomewizard/res/qak_org.diffscope.welcomewizard_actions.cpp" line="125"/>
+      <source>Welcome Wizard</source>
+      <translation>欢迎向导</translation>
+    </message>
+  </context>
+</TS>
